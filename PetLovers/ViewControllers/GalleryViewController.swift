69cb9340532fc4f63a41124baf7086da48c680dd
David Zhou <zhou@Davids-iMac.attlocal.net>
2023-03-08 16:32:18 -0800

    add folders

--- PetLovers/ViewControllers/GalleryViewController.swift
+++ PetLovers/ViewControllers/GalleryViewController.swift
@@ -13,9 +13,10 @@ class GalleryViewController: UIViewController {
         let label = UILabel(frame: CGRect(x: 100, y: 100, width: 1000, height: 100))
         label.textColor = UIColor.red
         label.backgroundColor = UIColor.white
-        label.text = "this is example app on 3/7/2023"
+        label.text = "this is doudou photo"
         
         
         self.view.addSubview(label)
+        navigationItem.title = "Doudou"
     }
 }
