93e2ba23852bdc95409569c2b4bc0d5b3cc40c49
David Zhou <zhou@Davids-iMac.attlocal.net>
2023-03-16 17:47:20 -0700

    set APIController

--- PetLovers/API/APIController.swift
+++ PetLovers/API/APIController.swift
@@ -8,16 +8,54 @@
 import Foundation
 
 class APIController {
-    func retrievePhotos(from cursor: UUID? = nil,
-                        completion: @escaping (Result<APIResponse, Swift.Error>) -> ()) {
-        var componnets = URLComponents(string: Constants.defaultBaseURL)
-        
-        //cursor
+    //TODO: rewrite
+    fileprivate let baseURL: String
+
+    init(baseURL: String = Constants.defaultBaseURL) {
+        self.baseURL = baseURL
     }
+
+    func retrievePhotos(from cursor: UUID? = nil, completion: @escaping (Result<APIResponse, Swift.Error>) -> ()) {
+        var components = URLComponents(string: Constants.defaultBaseURL)
+        components?.path = Constants.photosEndpoint
+        if let cursor = cursor {
+            components?.queryItems = [URLQueryItem(name: Constants.cursorParameter, value: cursor.uuidString)]
+        }
+        guard let url = components?.url else {
+            completion(.failure(Error.invalidURL))
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                guard let data = data else {
+                    completion(.failure(Error.invalidResponse))
+                    return
+                }
+
+                let decoded = try! decoder.decode(APIResponse.self, from: data)
+                completion(.success(decoded))
+            }
+        }
+        task.resume()
+    }
+    //TODO: rewrite
+
 }
 
 extension APIController {
+    //TODO: rewrite
     struct Constants {
         static let defaultBaseURL = "https://us-central1-kitunia.cloudfunctions.net"
+        static let photosEndpoint = "/photos"
+        static let cursorParameter = "cursor"
+    }
+
+    enum Error: Swift.Error {
+        case invalidURL
+        case invalidResponse
     }
 }
---
+++ PetLovers/Model/Photo.swift
@@ -0,0 +1,44 @@
+//
+//  Photo.swift
+//  PetLovers
+//
+//  Created by David Zhou on 3/16/23.
+//
+
+import Foundation
+
+struct Photo : Identifiable, Hashable, Equatable, Codable{
+    
+    let id: UUID
+    let size: Size
+    let urls: URLs
+    let timestamp: Date
+    let location: Location?
+    
+}
+
+//comparable
+extension Photo: Comparable {
+
+    static func < (lhs: Photo, rhs: Photo) -> Bool {
+        return lhs.timestamp < rhs.timestamp
+    }
+    
+}
+
+extension Photo {
+    struct URLs : Hashable, Equatable, Codable{
+        let full: URL
+        let thumb: URL
+    }
+    
+    struct Size : Hashable, Equatable, Codable{
+        let width: Int
+        let height: Int
+    }
+    
+    struct Location : Hashable, Equatable, Codable{
+        let lat: Double
+        let lng: Double
+    }
+}
