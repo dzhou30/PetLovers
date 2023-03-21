09d7b56953607c97503200cb85669e514580ac09
David Zhou <zhou@Davids-iMac.attlocal.net>
2023-03-21 15:38:25 -0700

    add table view cell file

---
+++ PetLovers/Cells/ListViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListViewCell.swift
+//  PetLovers
+//
+//  Created by David Zhou on 3/21/23.
+//
+
+import UIKit
+
+class ListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
