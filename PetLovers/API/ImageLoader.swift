5e0aa7d06b8052b456bb17fb94be0adc541df245
David Zhou <zhou@Davids-iMac.attlocal.net>
2023-03-21 11:49:48 -0700

    add cache to image loader

--- PetLovers/API/ImageLoader.swift
+++ PetLovers/API/ImageLoader.swift
@@ -11,6 +11,17 @@ import UIKit
 class ImageLoader {
     
     var tasks : [Photo: URLSessionDataTask] = [:]
+    //Cache #1
+    var imageCache = ImageCache(countLimit: 200)
+    
+    //Cache #2
+    init() {
+        let memoryCapacity = Int(Measurement(value: 300, unit: UnitInformationStorage.megabytes).converted(to: .bytes).value)
+        let memoryCapacity2 = 300000000
+        let diskCapacity = 1000000000
+        URLSession.shared.configuration.urlCache = URLCache(memoryCapacity: memoryCapacity2, diskCapacity: diskCapacity)
+        URLSession.shared.configuration.requestCachePolicy = .returnCacheDataElseLoad
+    }
     
     //load photo to UIImage
     func load(photo: Photo, size: Size, completion: @escaping( (UIImage?) -> ())) {
@@ -21,12 +32,20 @@ class ImageLoader {
         case .full:
             url = photo.urls.full
         }
+        if let image = imageCache.getImage(for: url) {
+            print("loaded from cache #1")
+            completion(image)
+            return
+        }
         let task = URLSession.shared.dataTask(with: url) { data, result, error in
             guard let data = data else {
                 completion(nil)
                 return
             }
             let image = UIImage(data: data)
+            if let loadedImage = image {
+                self.imageCache.insertImage(image: loadedImage, for: url)
+            }
             completion(image)
         }
         tasks[photo] = task
@@ -35,7 +54,41 @@ class ImageLoader {
 }
 
 extension ImageLoader {
+    
     enum Size {
         case thumb, full
+    }
+}
+
+class ImageCache {
+    
+    var cache = NSCache<AnyObject, AnyObject>()
+    let lock = NSLock()
+    var countLimit: Int
+    
+    init(countLimit: Int) {
+        self.countLimit = countLimit
+        cache.countLimit = countLimit
+    }
+    
+    func insertImage(image: UIImage, for url: URL) {
+        //lock.lock()
+        cache.setObject(image, forKey: url as AnyObject)
+        //lock.unlock()
+    }
+    
+    func removeImage(image: UIImage, for url: URL) {
+        //lock.lock()
+        cache.removeObject(forKey: url as AnyObject)
+        //lock.unlock()
+    }
+    
+    func getImage(for url: URL) -> UIImage? {
+        //lock.lock()
+        if let image = cache.object(forKey: url as AnyObject) as? UIImage {
+            return image
+        }
+        return nil
+        //lock.unlock()
     }
 }
